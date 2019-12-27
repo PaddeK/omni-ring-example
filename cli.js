6533024263de09d0e982016e14ce1da87be9e7a8
Padde <patrick_kloes@arcor.de>
2019-12-27 04:51:20 +0100

    - npx cli

--- cli.js
+++ cli.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log(process.argv);
+switch (process.argv.pop()) {
+    case 'convert':
+        require('./examples/convert');
+        break;
+    default:
+        require('./index');
+        break;
+}
+
